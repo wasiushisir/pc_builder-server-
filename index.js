c114f8d6c893dbb2686a63621caf40395a5319dc
wasiushisir <wasiuahmed410@gmail.com>
2023-09-05 22:05:28 +0600

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = 3000;
+const port = 443;
 const cors = require("cors");
 
 app.use(cors());
